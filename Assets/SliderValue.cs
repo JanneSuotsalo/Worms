0b3656f89cee1b526b145ba865b5fe4bf91cf436
maffimuffi <33652140+maffimuffi@users.noreply.github.com>
2017-11-15 22:14:14 +0200

    Pause
    Menu

---
+++ Assets/SliderValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour {
+
+	public Text sliderText;
+	public Slider slider;
+	public string prefix;
+
+	private void Update () {
+		sliderText.text = prefix + slider.value.ToString () + " / " + slider.maxValue;
+	}
+}
