66fef80d3e8796833678cc282063fab069977a69
JanneSuotsalo <janne.suotsalo@gmail.com>
2017-11-15 08:58:44 +0200

    as
    as

---
+++ Assets/RightBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightBullet : MonoBehaviour {
+
+	private Rigidbody2D rigidBodyR;
+
+	public Vector2 speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigidBodyR = GetComponent<Rigidbody2D> ();
+		rigidBodyR.velocity = speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rigidBodyR.velocity = speed;
+	}
+}
