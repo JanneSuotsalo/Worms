05cd4107a20f1db58ef1eafe605dfde8c15a6ff3
JanneSuotsalo <janne.suotsalo@gmail.com>
2017-11-17 11:49:48 +0200

    as
    as

--- Assets/Scrpts/PlayerController.cs
+++ Assets/Scrpts/PlayerController.cs
@@ -21,7 +21,10 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField]
 	protected LayerMask whatIsGround;
 
-	protected bool isGrounded, crouch, jump,facingRight, haveGun,shoot;
+	protected bool isGrounded, crouch, jump,facingRight;
+
+	protected bool shoot;
+	protected bool haveGun;
 
 	[SerializeField]
 	protected float jumpForce;
@@ -34,8 +37,8 @@ public class PlayerController : MonoBehaviour {
 
 	protected void HandleMovement(float horizontal) 
 	{
-		myRigibody.velocity = new Vector2 (horizontal * movementSpeed, myRigibody.velocity.y);
-		animator.SetFloat ("Speed", Mathf.Abs (horizontal));
+			myRigibody.velocity = new Vector2 (horizontal * movementSpeed, myRigibody.velocity.y);
+			animator.SetFloat ("Speed", Mathf.Abs (horizontal));
 
 		if (isGrounded && crouch && !shoot) {
 			isGrounded = true;
@@ -43,11 +46,24 @@ public class PlayerController : MonoBehaviour {
 			animator.SetBool ("Crouch", true);
 		}
 
-		if (isGrounded && jump) {
+		if (isGrounded && jump && !shoot) {
 			isGrounded = false;
 			jump = true;
 			myRigibody.AddForce(new Vector2(0,jumpForce));
 			animator.SetBool ("Jump", true);
+		}
+		//if (isGrounded && jump && shoot) {
+		//	isGrounded = false;
+		//	jump = true;
+			//myRigibody.AddForce(new Vector2(0,jumpForce));
+			//animator.SetBool ("JumpShoot", true);
+		//
+
+		if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && haveGun && !shoot) {
+			animator.Play ("IdleGun");
+		}
+		if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && !haveGun && !shoot) {
+			animator.Play ("Player Idle");
 		}
 	  }
 
@@ -73,32 +89,37 @@ public class PlayerController : MonoBehaviour {
 
 	protected void Shoot()
 		{
-		if(time <= 0) {
-			if (facingRight) {
-				Instantiate (RightBullet, shootingPos.position, Quaternion.identity);
-			}
-			if (facingRight && crouch && haveGun) {
-				Instantiate (RightBullet, shootingPos.position, Quaternion.identity);
-				animator.SetBool ("CrouchShoot", true);
-			}
+		if(haveGun) {
+			if (time <= 0) {
+				if (facingRight) {
+					Instantiate (RightBullet, shootingPos.position, Quaternion.identity);
+				}
+				if (facingRight && crouch && haveGun) {
+					Instantiate (RightBullet, shootingPos.position, Quaternion.identity);
+					animator.SetBool ("CrouchShoot", true);
+				}
+				if (facingRight && !crouch && haveGun) {
+					Instantiate (RightBullet, shootingPos.position, Quaternion.identity);
+					animator.SetBool ("CrouchShoot", false);
+				}
 
-			if (!facingRight && crouch && haveGun) {
-				Instantiate (LeftBullet, shootingPos.position, Quaternion.identity);
-				animator.SetBool ("CrouchShoot", true);
-			}
-			if (!facingRight) {
-				Instantiate (LeftBullet, shootingPos.position, Quaternion.identity);
-			}
+				if (!facingRight && crouch) {
+					Instantiate (LeftBullet, shootingPos.position, Quaternion.identity);
+					animator.SetBool ("CrouchShoot", true);
+				}
+				if (!facingRight && !crouch) {
+					Instantiate (LeftBullet, shootingPos.position, Quaternion.identity);
+					animator.SetBool ("CrouchShoot", false);
+				}
+
+				if (!facingRight) {
+					Instantiate (LeftBullet, shootingPos.position, Quaternion.identity);
+				}
+				}
+				if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && haveGun && shoot) {
+					animator.Play ("PlayerIdleShoot");
+				}
 			time = 0.2f;
-		}
-		if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && haveGun && shoot) {
-			animator.Play ("PlayerIdleShoot");
-		}
-		if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && haveGun && !shoot) {
-			animator.Play ("IdleGun");
-		}
-		if (isGrounded && !jump && !crouch && myRigibody.velocity.x == 0 && !haveGun && !shoot) {
-			animator.Play ("Player Idle");
-		}
+			}
 	}
 }
--- Assets/Scrpts/RightBullet.cs
+++ Assets/Scrpts/RightBullet.cs
@@ -8,16 +8,30 @@ public class RightBullet : MonoBehaviour {
 
 	public Vector2 speed;
 
+	public LayerMask notToHit;
+
 	// Use this for initialization
 	void Start () 
 	{
 		rigidBodyR = GetComponent<Rigidbody2D> ();
 		rigidBodyR.velocity = speed;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		rigidBodyR.velocity = speed;
+
+	}
+	void OnTriggerEnter(Collider2D col)
+	{
+		//all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+		if (col.gameObject.tag == "Enemy") {
+			Destroy (col.gameObject);
+			//add an explosion or something
+			//destroy the projectile that just caused the trigger collision
+			Destroy (gameObject);
+		}
 	}
 }
