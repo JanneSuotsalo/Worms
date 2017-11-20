ba63d55f6065a8e664632e40aaebfea8f0171799
maffimuffi <33652140+maffimuffi@users.noreply.github.com>
2017-11-20 14:01:09 +0200

    sdf

--- Assets/Scrpts/Pause.cs
+++ Assets/Scrpts/Pause.cs
@@ -45,82 +45,9 @@ public class Pause : MonoBehaviour
 
 	public void Restart () {
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
-		UnPause ();
 	}
+
+//	public void Exit () {
+//		SceneManager.LoadScene (SceneManager.LoadScene (0));
+//	}
 }
-
-//	void Start ()
-//	{
-//		scene = SceneManager.GetActiveScene ();
-//		MenuButton.onClick.AddListener (OnPause);
-//
-//	}
-//
-//	void Update ()
-//	{
-//		if (!Menu.activeSelf && Time.timeScale == 0) {
-//			UnPause ();
-//		}
-//		if (Menu.activeSelf && Time.timeScale == 1) {
-//			OnPause ();
-//		}
-//	}
-//
-//	public void OnPause ()
-//	{
-//		Time.timeScale = 0;
-//	}
-//
-//	public void UnPause ()
-//	{
-//		Time.timeScale = 1;
-//	}
-//
-//	public void Restart ()
-//	{
-//		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
-//	}
-//}
-
-//	public void Exit ()
-//	{
-//		Application.Quit ();
-//	}
-
-
-
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//
-//public class Pause : MonoBehaviour
-//{
-//
-//	private bool paused;
-//	public GameObject Menu;
-//	public Button MenuButton;
-//
-//	void Start ()
-//	{
-//		paused = false;
-//		MenuButton.onClick.AddListener (OnPause);
-//
-//	}
-//
-//	void Update ()
-//	{
-//		if (!Menu.activeSelf && Time.timeScale == 0) {
-//			UnPause ();
-//		}
-//	}
-//
-//	public void OnPause ()
-//	{
-//		Time.timeScale = 0;
-//	}
-//
-//	public void UnPause ()
-//	{
-//		Time.timeScale = 1;
-//	}
-//}
