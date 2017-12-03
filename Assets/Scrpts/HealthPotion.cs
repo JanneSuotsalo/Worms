f7b6c9d069555a763f609cd5d287dbcccef80b3f
JanneSuotsalo <janne.suotsalo@gmail.com>
2017-12-03 23:34:00 +0200

    as
    as

---
+++ Assets/Scrpts/HealthPotion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour {
+
+	public int heal;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			col.gameObject.GetComponent<Player> ().HealPlayer (heal);
+			Destroy (gameObject);
+		}
+	}
+}
